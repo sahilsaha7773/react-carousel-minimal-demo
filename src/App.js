927d618e6f227e9c1b8cae8076d11c1235476051
sahilsaha7773 <sahilsaha7773@gmail.com>
2021-08-07 15:40:32 +0530

    Initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { Carousel } from 'react-carousel-minimal';
 
 function App() {
+  const data = [
+    {
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/GoldenGateBridge-001.jpg/1200px-GoldenGateBridge-001.jpg",
+      caption: "San Francisco"
+    },
+    {
+      image: "https://cdn.britannica.com/s:800x450,c:crop/35/204435-138-2F2B745A/Time-lapse-hyper-lapse-Isle-Skye-Scotland.jpg",
+      caption: "Scotland"
+    },
+    {
+      image: "https://static2.tripoto.com/media/filter/tst/img/735873/TripDocument/1537686560_1537686557954.jpg",
+      caption: "Darjeeling"
+    }
+  ];
+
+  const captionStyle = {
+    fontSize: '1.5em',
+    fontWeight: 'bold',
+  }
+  const slideNumberStyle = {
+    fontSize: '20px',
+    fontWeight: 'bold',
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ textAlign: "center" }}>
+        <h2>React Carousel Minimal</h2>
+        <p>Easy to use, responsive and customizable carousel component for React Projects.</p>
+        <div style={{
+          padding: "0 20px"
+        }}>
+          <Carousel
+            data={data}
+            time={2000}
+            width="650px"
+            height="400px"
+            captionStyle={captionStyle}
+            radius="10px"
+            slideNumber={true}
+            slideNumberStyle={slideNumberStyle}
+            captionPosition="bottom"
+            automatic={true}
+            dots={true}
+            pauseIconColor="white"
+            pauseIconSize="40px"
+            slideBackgroundColor="darkgrey"
+            slideImageFit="cover"
+            style={{
+              textAlign: "center",
+              maxWidth: "600px",
+              maxHeight: "400px",
+              margin: "40px auto",
+            }}
+          />
+        </div>
+
+        {/* <h3>Features</h3>
+        <ul>
+          <li>Responsive</li>
+          Responsive
+          Customizable
+          Infinite loop
+          Autoplay with custom duration
+          Supports text caption
+          Pause autoplay on hold
+          Slide number indicators
+
+        </ul> */}
+      </div>
     </div>
   );
 }

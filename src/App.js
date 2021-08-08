c3bb748a34e516ac5d6cf05e382f70fdcf0abecc
Sahil Saha <sahilsaha7773@gmail.com>
2021-08-08 18:55:26 +0530

    Added settings and other UIs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
+import { useState } from 'react';
 import { Carousel } from 'react-carousel-minimal';
+import GithubCorner from 'react-github-corner';
+import { Fork, Sponsor, Star, UsedBy, Watch } from 'react-github-buttons';
+import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+
+import './App.css';
+import SyntaxHighlighter from 'react-syntax-highlighter/dist/esm/default-highlight';
 
 function App() {
   const data = [
@@ -39,6 +46,20 @@ function App() {
       caption: "Darjeeling"
     }
   ];
+  const [time, setTime] = useState(2000);
+  const [width, setWidth] = useState("850px");
+  const [height, setHeight] = useState("500px");
+  const [radius, setRadius] = useState("10px");
+  const [slideNumber, setSlideNumber] = useState(true);
+  const [captionPosition, setCaptionPosition] = useState("bottom");
+  const [automatic, setAutomatic] = useState(true);
+  const [dots, setDots] = useState(true);
+  const [pauseIconColor, setPauseIconColor] = useState("white");
+  const [pauseIconSize, setPauseIconSize] = useState("30px");
+  const [slideBackgroundColor, setSlideBackgroundColor] = useState("darkgrey");
+  const [slideImageFit, setSlideImageFit] = useState("cover");
+  const [thumbnails, setThumbnails] = useState(true);
+  const [thumbnailWidth, setThumbnailWidth] = useState("100px");
 
   const captionStyle = {
     fontSize: '2em',
@@ -50,39 +71,104 @@ function App() {
   }
   return (
     <div className="App">
-      <div style={{ textAlign: "center" }}>
+      <GithubCorner href="https://github.com/sahilsaha7773/react-carousel-minimal" />
+      <div style={{ textAlign: "center", maxWidth: "850px", margin: "auto" }}>
         <h2>React Carousel Minimal</h2>
         <p>Easy to use, responsive and customizable carousel component for React Projects.</p>
         <div style={{
-          padding: "0 20px"
+          display: "flex",
+          justifyContent: "center",
+        }}>
+          <Star owner="sahilsaha7773" repo="react-carousel-minimal" />
+          <Fork owner="sahilsaha7773" repo="react-carousel-minimal" />
+          <a href="https://www.npmjs.com/package/react-carousel-minimal" target="_blank" className="npm-btn">npm</a>
+          {/* <Sponsor owner="sahilsaha7773" repo="react-carousel-minimal" /> */}
+        </div>
+        <div style={{
+          margin: "20px",
+        }}>
+
+          <SyntaxHighlighter>
+            npm i react-carousel-minimal
+          </SyntaxHighlighter>
+        </div>
+        <div style={{
+          padding: "0 20px",
         }}>
           <Carousel
             data={data}
-            time={2000}
-            width="850px"
-            height="500px"
+            time={time}
+            width={width}
+            height={height}
             captionStyle={captionStyle}
-            radius="10px"
-            slideNumber={true}
+            radius={radius}
+            slideNumber={slideNumber}
             slideNumberStyle={slideNumberStyle}
-            captionPosition="bottom"
-            automatic={true}
-            dots={true}
-            pauseIconColor="white"
-            pauseIconSize="40px"
-            slideBackgroundColor="darkgrey"
-            slideImageFit="cover"
-            thumbnails={true}
-            thumbnailWidth="100px"
+            captionPosition={captionPosition}
+            automatic={automatic}
+            dots={dots}
+            pauseIconColor={pauseIconColor}
+            pauseIconSize={pauseIconSize}
+            slideBackgroundColor={slideBackgroundColor}
+            slideImageFit={slideImageFit}
+            thumbnails={thumbnails}
+            thumbnailWidth={thumbnailWidth}
             style={{
               textAlign: "center",
               maxWidth: "850px",
-              maxHeight: "500px",
               margin: "40px auto",
             }}
           />
+          <h2 style={{ marginBottom: "10px" }}>Settings</h2>
+          <p style={{ margin: "10px 0 20px 0" }}>All props are not shown here</p>
+          {/* <input type="text" aria-label="time" value={time} onChange={e => setTime(e.target.value)} /> */}
+          <form>
+            <div className="form-element" style={{ display: "block" }}>
+              <label>Radius:</label>
+              <div className="inp-box">
+                <input type="text" value={radius} width="100%" onChange={e => setRadius(e.target.value)} />
+              </div>
+            </div>
+            <div className="form-element" style={{ display: "block" }}>
+              <label>Caption Position: </label>
+              <div className="inp-box">
+                <select value={captionPosition} onChange={(e) => setCaptionPosition(e.target.value)}>
+                  <option value="top">Top</option>
+                  <option value="center">Center</option>
+                  <option value="bottom">Bottom</option>
+                </select>
+              </div>
+            </div>
+            {/* <div className="form-element">
+              <label>Automatic: </label>
+              <input type="checkbox" checked={automatic} onChange={e => setAutomatic(e.target.checked)} />
+            </div> */}
+            <div className="form-element">
+
+              <div className="inp-box">
+                <label>Thumbnails: </label>
+                <input type="checkbox" checked={thumbnails} onChange={e => setThumbnails(e.target.checked)} />
+              </div>
+            </div>
+            <div className="form-element">
+
+              <div className="inp-box">
+                <label>Slide Number: </label>
+                <input type="checkbox" checked={slideNumber} onChange={e => setSlideNumber(e.target.checked)} />
+              </div>
+            </div>
+            <div className="form-element">
+              <div className="inp-box">
+
+                <label>Dots: </label>
+                <input type="checkbox" checked={dots} onChange={e => setDots(e.target.checked)} />
+              </div>
+            </div>
+
+          </form>
         </div>
       </div>
+      <p style={{ textAlign: "center", margin: "20px 0 40px 0" }}>Made with ❤️ by <a target="_blank" href="http://sahilsaha.me">Sahil Saha</a></p>
     </div>
   );
 }
